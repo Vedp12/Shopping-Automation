c5bac4e3158e6a05feb772b90e24007765a980e8
Ved Patel <ved00684@gmail.com>
2026-07-31 17:46:02 +0530

    automtion

---
+++ auto_table.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
+
+url = "https://www.football-data.co.uk/englandm.php"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+response = requests.get(url, headers=headers,timeout=10)
+soap = bs(response.text,"html.parser")
+all_anchor = soap.find_all('a',href=True)
+
+for anchor in all_anchor:
+    if anchor['href'].lower().endswith('.csv'):
+        csv_url = urljoin(url, anchor['href'])
+
+        csv_Data = pd.read_csv(csv_url) 
+        print(csv_url)
+        #a = pd.read_csv(andhor[.c])
+#tables = pd.read_csv(url)
+
+#a = pd.read_csv('https://www.football-data.co.uk/mmz4281/2526/E0.csv')
+#a.rename(columns={'FTHG':'Home_goals','FTAG':'away_goals'},inplace=True)
+#print(a)
---
+++ table_extractor.py
@@ -0,0 +1,7 @@
+import camelot
+
+tables = camelot.read_pdf('Computer_Networks.pdf',pages='1')
+print(tables)
+
+tables.export('Computer_Networks.csv',f='csv',compress=True)
+tables[0].to_csv('Computer_Networks.csv')

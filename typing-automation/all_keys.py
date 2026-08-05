c9dceb3cd2edf135d8599d89553968a36eb68d36
Ved Patel <ved00684@gmail.com>
2026-08-05 23:08:32 +0530

    Unncessary module removed

--- typing-automation/all_keys.py
+++ typing-automation/all_keys.py
@@ -1,14 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webdriver import import_cdp
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import os
 import time
 import string
-import random
 
 service = Service("/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
